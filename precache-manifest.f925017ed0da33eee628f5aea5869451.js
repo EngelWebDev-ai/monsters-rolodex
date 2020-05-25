010d381ea7a73ea2b5288e56c1662ac2ae7822e2
Rutger <fatherengel@gmail.com>
2020-05-25 15:20:02 +0200

    Updates

---
+++ precache-manifest.f925017ed0da33eee628f5aea5869451.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e152d23620ee150849a1f53c0df444b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b9ce1e94710512c0a427",
+    "url": "/monsters-rolodex/static/css/main.77fbe5a0.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9ce1e94710512c0a427",
+    "url": "/monsters-rolodex/static/js/main.b6caba1e.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
